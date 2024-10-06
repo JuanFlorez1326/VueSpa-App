6bb65a175e23279e5f13b45095a33d6847a7a288
Juan Florez <juanks339@gmail.com>
2024-10-05 21:54:19 -0500

    NEW: Vue-Spa APP

---
+++ src/router/index.ts
@@ -0,0 +1,30 @@
+import HomePage from '@/modules/landing/pages/HomePage.vue';
+import { createRouter, createWebHashHistory } from 'vue-router';
+
+const router = createRouter({
+  history: createWebHashHistory(import.meta.env.BASE_URL),
+  routes: [
+    {
+      path: '/',
+      name: 'home',
+      component: HomePage,
+    },
+    {
+      path: '/feactures',
+      name: 'feactures',
+      component: () => import('@/modules/landing/pages/FeacturesPages.vue'),
+    },
+    {
+      path: '/pricing',
+      name: 'pricing',
+      component: () => import('@/modules/landing/pages/PricingPage.vue'),
+    },
+    {
+      path: '/contact',
+      name: 'contact',
+      component: () => import('@/modules/landing/pages/ContactPage.vue'),
+    },
+  ],
+});
+
+export default router;
